d2ba6567e6cecadac6f073fb50156d60abc87ad6
Prashantgarg14072001 <114285431+Prashantgarg14072001@users.noreply.github.com>
2022-09-24 15:46:53 +0530

    Update n4.php

--- n4.php
+++ n4.php
@@ -9,7 +9,7 @@
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
   <meta charset="utf-8">
-  <title>Welcome To JyoPra Canteen</title>
+  <title>Welcome To YamWay Canteen</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
